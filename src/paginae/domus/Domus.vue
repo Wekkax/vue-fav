b3ba81eb32ba1227fc9f36cd5bf488c7b2956096
Wekkax <126560061+Wekkax@users.noreply.github.com>
2026-03-25 17:45:32 +0100

    mmanalf: Inicio de la app con Tailwind, Shadcn y Vue router

---
+++ src/paginae/domus/Domus.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+
+</script>
+
+
+<template>
+
+<div class="cappa">
+  <h1>Home Page</h1>
+  
+  <Button variant="destructive" class="bg-green-500 px-12 py-8 text-3xl hover:bg-green-700">
+    Click me
+  </Button>
+</div>
+  
+</template>
+
+<style scoped>
+.cappa {
+  margin: 20px;
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 12px;
+}
+
+</style>
+
