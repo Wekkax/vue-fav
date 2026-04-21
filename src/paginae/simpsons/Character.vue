85bf3a86073e9d594b68989c3661af2a458b0f51
Wekkax <126560061+Wekkax@users.noreply.github.com>
2026-04-21 18:23:58 +0200

    mmanalf: Rutas dinámicas y anidadas

---
+++ src/paginae/simpsons/Character.vue
@@ -0,0 +1,14 @@
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>Página de Personaje</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/paginae/simpsons/Layout.vue
@@ -0,0 +1,14 @@
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>Layout para Circa, etc, de la ruta Simpsons</h1>
+    <main>
+        <h1>Inicio Layout</h1>
+        <RouterView />
+        <h1>Fin Layout</h1>
+    </main>
+</template>
+
